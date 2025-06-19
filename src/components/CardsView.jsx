0dab2f0bb70b5e4c956b503ed9cd6c6dccbaab86
thekey404 <yashvalecha133@gmail.com>
2025-06-19 19:13:16 +0530

    commit2

--- src/components/CardsView.jsx
+++ src/components/CardsView.jsx
@@ -12,7 +12,7 @@ const CardsView = () => {
                 <div className='text-7xl text-center pb-6'>Projects</div>
                 <span className="absolute left-[40%] bottom-0 pb-1 w-64 h-[2px] bg-gradient-to-r from-pink-500 to-violet-500 scale-x-0 group-hover:scale-x-100 transition-transform origin-left duration-300"></span>
             </motion.a >
-            <div className="grid grid-cols-3 mx-8 mt-20">
+            <div className="grid grid-cols-3 mx-auto mt-20">
                 <div> <PixelCard variant="pink" /><div className='text-white mt-2 text-center text-2xl'>Project1</div></div>
                 <div> <PixelCard variant="pink" /><div className='text-white mt-2 text-center text-2xl'>Project2</div></div>
                 <div> <PixelCard variant="pink" /><div className='text-white mt-2 text-center text-2xl'>Project3</div></div>
