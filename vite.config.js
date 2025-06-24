cac7dbb2c20d57f770c104370d4e98aa882bd749
thekey404 <yashvalecha133@gmail.com>
2025-06-24 12:47:19 +0530

    Fix: Rename postcss config file for Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   base: '/Portfolio.V1/',
-   build: {
+  build: {
     chunkSizeWarningLimit: 1500
-   }
+  }
 })
 
 
